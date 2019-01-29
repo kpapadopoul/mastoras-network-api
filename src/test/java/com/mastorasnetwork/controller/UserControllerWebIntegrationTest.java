a5ae7044b7b471516a7e1edabf4998bc7a2bfaf5
Konstantinos Papadopoulos <Konstantinos.Papadopoulos@IRIworldwide.com>
2019-01-29 17:24:30 +0200

    Create First Web Integration Test.

---
+++ src/test/java/com/mastorasnetwork/controller/UserControllerWebIntegrationTest.java
@@ -0,0 +1,40 @@
+package com.mastorasnetwork.controller;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class UserControllerWebIntegrationTest {
+
+    @Test
+    void list() throws IOException {
+
+        TestRestTemplate restTemplate = new TestRestTemplate();
+        ResponseEntity<String> response = restTemplate.getForEntity(
+                "http://localhost:8080/api/v1/users", String.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode responseJson = objectMapper.readTree(response.getBody());
+
+        assertThat(responseJson.isMissingNode(), is(false));
+        assertThat(responseJson.toString(), equalTo("[]"));
+
+    }
+
+}
