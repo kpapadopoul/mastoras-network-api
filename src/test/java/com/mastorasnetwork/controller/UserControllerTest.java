52ea66fc9e0feff48b0feb86b1d15030b1981137
Konstantinos Papadopoulos <Konstantinos.Papadopoulos@IRIworldwide.com>
2019-01-30 11:23:34 +0200

    MN-15: Create unit tests for the remaining methods of UserController.

--- src/test/java/com/mastorasnetwork/controller/UserControllerTest.java
+++ src/test/java/com/mastorasnetwork/controller/UserControllerTest.java
@@ -8,12 +8,17 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
 
 class UserControllerTest {
 
@@ -31,10 +36,32 @@ class UserControllerTest {
     @Test
     void list() {
 
+        final List<User> expectedUsers = prepareUsers();
+        when(userRepository.findAll()).thenReturn(expectedUsers);
+
+        final List<User> users = userController.list();
+
+        verify(userRepository).findAll();
+        assertThat(users, is(notNullValue()));
+        assertThat(users.size(), is(greaterThan(0)));
+        assertThat(users.size(), is(expectedUsers.size()));
+        assertThat(users, is(expectedUsers));
+
     }
 
     @Test
     void create() {
+
+        User inputUser = new User();
+        inputUser.setId(1L);
+        when(userRepository.saveAndFlush(inputUser)).thenReturn(inputUser);
+
+        User outputUser = userController.create(inputUser);
+
+        verify(userRepository).saveAndFlush(inputUser);
+        assertThat(outputUser.getId(), is(1L));
+        assertThat(outputUser, is(inputUser));
+
     }
 
     @Test
@@ -50,14 +77,57 @@ class UserControllerTest {
 
         verify(userRepository).findById(1L);
         assertThat(user.getId(), is(1L));
+        assertThat(user, is(expectedUser));
 
     }
 
     @Test
     void update() {
+
+        final List<User> users = prepareUsers();
+        User existingUser = users.get(0);
+        User expectedUser = users.get(1);
+
+        when(userRepository.findById(1L)).thenReturn(Optional.of(existingUser));
+        when(userRepository.saveAndFlush(existingUser)).thenReturn(expectedUser);
+
+        final User user = userController.update(1L, existingUser);
+
+        verify(userRepository).findById(1L);
+        verify(userRepository).saveAndFlush(existingUser);
+        assertThat(user.getId(), is(2L));
+        assertThat(user, is(expectedUser));
+
     }
 
     @Test
     void delete() {
+
+        User expectedUser = new User();
+        expectedUser.setId(1L);
+
+        when(userRepository.findById(1L)).thenReturn(Optional.of(expectedUser));
+        doAnswer((i) -> null).when(userRepository).delete(expectedUser);
+
+        final User user = userController.delete(1L);
+
+        verify(userRepository).findById(1L);
+        verify(userRepository).delete(expectedUser);
+        assertThat(user.getId(), is(1L));
+        assertThat(user, is(expectedUser));
+
     }
+
+    private List<User> prepareUsers() {
+
+        User user1 = new User();
+        user1.setId(1L);
+
+        User user2 = new User();
+        user2.setId(2L);
+
+        return Arrays.asList(user1, user2);
+
+    }
+
 }
